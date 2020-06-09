9c82cbaf76f58090449959c5d48e0a3b20bfa7c4
ergouwang <mrergouwang@gmail.com>
2020-06-09 19:26:52 +0800

    dart-scss 配置完成

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
+  // 这个标签没有实际的功能，主要是检查你的代码哪里有用错的地方，主要是用来纠正你的代码
   <React.StrictMode>
     <App />
   </React.StrictMode>,
