4975f93d44fae52a8996f652460a87cfe6848a06
ergouwang <mrergouwang@gmail.com>
2020-06-10 11:50:24 +0800

    完成底部导航

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,110 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
+import styled from 'styled-components'
+import {
+  //hash 模式
+  HashRouter as Router,
+  //history模式
+  // BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+const Wrapper = styled.div`
+  border:1px solid red;
+  height:100vh;
+  display:flex;
+  flex-direction:column;
+`;
+
+const Main = styled.div`
+  border:1px solid green;
+  flex-grow:1;
+`;
+
+const Nav = styled.div`
+  flex-direction:row;
+  >ul{
+    display:flex;
+    >li{
+      width:33.333%;
+      text-align:center;
+      padding:16px;
+    }
+  }
+
+`;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Route exact path="/">
+              <Tags />
+            </Route>
+            <Route path="/money">
+              <Money />
+            </Route>
+            <Route path="/statistics">
+              <Statistics />
+            </Route>
+            <Route path="*">
+              <NoMatch />
+            </Route>
+          </Switch>
+        </Main>
+        <Nav>
+          <ul>
+            <li>
+              <Link to="/tags">标签页</Link>
+            </li>
+            <li>
+              <Link to="/money">记账页</Link>
+            </li>
+            <li>
+              <Link to="/statistics">数据页</Link>
+            </li>
+          </ul>
+        </Nav>
+
+      </Wrapper>
+    </Router>
+  );
+}
+
+
+function Tags() {
+  return (
+    <div>
+      <h2>标签页</h2>
     </div>
   );
+}
+
+function Money() {
+  return (
+    <div>
+      <h2>记账页</h2>
+    </div>
+  );
+}
+
+function Statistics() {
+  return (
+    <div>
+      <h2>数据页</h2>
+    </div>
+  );
+}
+
+function NoMatch() {
+  return (
+    <div>页面不存在，请输入正确的地址哦~</div>
+  )
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'index.scss'
 
 ReactDOM.render(
-  // 这个标签没有实际的功能，主要是检查你的代码哪里有用错的地方，主要是用来纠正你的代码
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
