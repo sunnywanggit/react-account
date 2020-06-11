f794dbd33a9b7eaa8fbc57af9d4d416aa24e436b
ergouwang <mrergouwang@gmail.com>
2020-06-11 18:17:46 +0800

    创建views,并对页面进行封装

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.scss';
-import styled from 'styled-components'
 import {
   //hash 模式
   HashRouter as Router,
@@ -10,8 +9,10 @@ import {
   Route,
 } from "react-router-dom";
 
-import Nav from './components/Nav'
-import Layout from 'components/Layout';
+import Tags from 'views/Tags';
+import Money from 'views/Money';
+import Statistics from 'views/Statistics';
+import NoMatch from 'views/NoMatch';
 
 
 
@@ -37,36 +38,4 @@ function App() {
     </Router>
   );
 }
-
-
-function Tags() {
-  return (
-    <Layout>
-      <h2>标签页</h2>
-    </Layout>
-  );
-}
-
-function Money() {
-  return (
-    <Layout>
-      <h2>记账页</h2>
-    </Layout>
-  );
-}
-
-function Statistics() {
-  return (
-    <Layout>
-      <h2>数据页</h2>
-    </Layout>
-  );
-}
-
-function NoMatch() {
-  return (
-    <div>页面不存在，请输入正确的地址哦~</div>
-  )
-}
-
 export default App;
---
+++ src/views/NoMatch.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const NoMatch = () => {
+  return (
+    <div>页面不存在，请输入正确的地址哦~</div>
+  )
+}
+
+export default NoMatch;
